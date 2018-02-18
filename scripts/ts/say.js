16aef7b9b509ff87d143a49024cd0a6eb17cc881
jvlessa <jvcadastro@gmail.com>
2018-02-18 20:51:45 -0300

    Class

--- scripts/ts/say.js
+++ scripts/ts/say.js
@@ -1,7 +1,18 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, LastName) {
+        this.firstName = firstName;
+        this.LastName = LastName;
+        this.fullname = firstName + " " + LastName;
+    }
+    return Student;
+}());
+//function say(person: iPerson) - Line type removed because I have a class too
 function say(person) {
     return "Hello " + person.firstname;
 }
 var user = { firstname: "João Vitor", lastname: "Lessa" };
 //var user = {firstname: 10, lastname: 11};
 //That would cause error because TypeScript compiler would detected that variable is not a string
+var user2 = new Student("João Vitor", "Barros");
 console.log(say(user));
+console.log(say(user2));
--- scripts/ts/say.ts
+++ scripts/ts/say.ts
@@ -1,15 +1,25 @@
+class Student{
+    fullname: string;
+    constructor(public firstName, public LastName){
+        this.fullname = firstName + " " + LastName;
+    }
+}
+
 interface iPerson{
     firstname: string;
     lastname: string;
 }
 
-function say(person: iPerson){
+//function say(person: iPerson) - Line type removed because I have a class too
+function say(person){
     return "Hello " + person.firstname;
 }
 
 var user = {firstname: "João Vitor", lastname: "Lessa"};
-
 //var user = {firstname: 10, lastname: 11};
 //That would cause error because TypeScript compiler would detected that variable is not a string
 
+var user2 = new Student("João Vitor", "Barros");
+
 console.log(say(user));
+console.log(say(user2));
